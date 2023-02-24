83823fa4253c3eac7a9ae26f804c9dc6903e10fa
axelnoche <105469738+axelnoche@users.noreply.github.com>
2023-02-23 23:56:14 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,63 @@
+x = 0;
+y = 0;
+draw_circle  = "";
+draw_rect = "";
+
+var SpeechRecognition = window.webkitSpeechRecognition;
+
+var recognition = new SpeechRecognition();
+
+
+function start() {
+   document.getElementById("status").innerHTML = "El sistema está escuchando. Por favor, habla.";
+ recognition.start(); }
+
+
+recognition.onresult = function (event){
+
+    console.log(event);
+
+    var content = event.results[0][0].transcript;
+
+    document.getElementById("status").innerHTML = "La voz se reconocio como : "+content;
+    //cambie la  "C" de circle a minuscula para que coincida con la variable
+    if(content == "circle")
+      {
+        x = Math.floor(Math.random() * 900);
+        y = Math.floor(Math.random() * 600);
+        document.getElementById("status").innerHTML = "Se empezo a dibujar un círculo";
+        draw_circle = "set";
+      }
+      //cambie la  "R" de rectangle a minuscula para que coincida con la variable
+      if(content == "rectangle")
+      {
+        x = Math.floor(Math.random() * 900);
+        y = Math.floor(Math.random() * 600);
+        document.getElementById("status").innerHTML = "Se empezo a dibujar un rectángulo";
+        draw_rect = "set";
+      }
+      }
+
+      function setup(){
+         canvas = createCanvas(900,600);
+      }
+
+      function draw(){
+         if(draw_circle == "set")
+         {
+           radius = Math.floor (Math.random()*100);
+           circle(x,y, radius);
+           document.getElementById("status").innerHTML = "Se dibujo un circúlo.";
+           draw_circle = ""
+      }
+          if(draw_rect == "set")
+         {
+          // esta linea sobraba por que el rectangulo no necesita radio
+           rect(x,y,70,50);
+           document.getElementById("status").innerHTML = "Se dibujó un rectángulo. ";
+           draw_rect = "";
+      }
+
+      }
+
+      // todos los random estaban escritos como ramdom
